6ea1b4661f69e06f77fcdd72a454c93be58f7682
Tanya_02 <tr3520@bk.ru>
2024-10-12 12:27:18 +0300

    Created models for apps and made migrations

---
+++ materials/models.py
@@ -0,0 +1,70 @@
+from django.db import models
+
+class Course(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Название курса",
+        help_text="Введите название курса",
+    )
+    description = models.TextField(
+        verbose_name="Описание курса",
+        help_text="Введите описание курса"
+    )
+    preview = models.ImageField(
+        upload_to='materials/preview',
+        blank=True,
+        null=True,
+        verbose_name="Превью",
+        help_text="Загрузите превью курса",
+    )
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Class(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Название урока",
+        help_text="Введите название урока",
+    )
+    description = models.TextField(
+        verbose_name="Описание урока",
+        help_text="Введите описание урока"
+    )
+    image = models.ImageField(
+        upload_to="product/photo",
+        blank=True,
+        null=True,
+        verbose_name="Превью",
+        help_text="Загрузите фото продукта",
+    )
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        verbose_name="Название курса",
+        help_text="Введите название курса",
+        null=True,
+        blank=True,
+        related_name="courses",
+    )
+    url = models.URLField(
+        max_length=500,
+        null=True,
+        blank=True,
+        verbose_name="Ссылка нв урок",
+        help_text="Укажите ссылку на урок",
+    )
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
+        ordering = ["name", "course"]
+
+    def __str__(self):
+        return self.name
---
+++ users/models.py
@@ -0,0 +1,47 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(
+        unique=True,
+        verbose_name="Email",
+        help_text="Введите Ваш email"
+    )
+    avatar = models.ImageField(
+        upload_to='users/avatars',
+        blank=True,
+        null=True,
+        verbose_name="Фото",
+        help_text="Загрузите ваше фото",
+    )
+    phone = models.CharField(
+        max_length=35,
+        blank=True,
+        null=True,
+        verbose_name='Номер телефона',
+        help_text='Введите ваш номер',
+    )
+    city = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True,
+        verbose_name='Город',
+        help_text='Укажите город',
+    )
+    # token = models.CharField(
+    #     max_length=100,
+    #     blank=True,
+    #     null=True,
+    #     verbose_name='Token'
+    # )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.email
