29e2330b4237c88579ee1fee7e43ca8f67c87a61
Tanya_02 <tr3520@bk.ru>
2024-10-12 13:18:43 +0300

    Created viewsets and serializators for courses

---
+++ materials/views.py
@@ -0,0 +1,10 @@
+from rest_framework.viewsets import ModelViewSet
+from django.shortcuts import render
+
+from materials.models import Course
+from materials.serializers import CourseSerializer
+
+
+class CourseViewSet(ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
